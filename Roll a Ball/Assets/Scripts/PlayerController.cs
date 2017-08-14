4c99e5b3c62188224c9fedcd6d27c4304d2d6507
vv-b-s <valentin.v1997@gmail.com>
2017-08-15 01:11:05 +0300

    Added: Roll a Ball Tutorial (incomplete - creating walls state, empty walls object created.)

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);            // (x,y,z)
+
+        rb.AddForce(movement * speed);
+    }
+}
