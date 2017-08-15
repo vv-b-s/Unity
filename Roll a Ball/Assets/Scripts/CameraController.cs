56f5f0953a71703e737bd63c507c0508e4fe29c7
vv-b-s <valentin.v1997@gmail.com>
2017-08-16 00:43:37 +0300

    Making some changes to the camera script. Unsuccessfull rotation

--- Roll a Ball/Assets/Scripts/CameraController.cs
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -1,19 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    #region Game Objects
+    public GameObject Player;
+    public GameObject WestWall;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-        offset = transform.position - player.transform.position;
+    #region Offsets
+    private Vector3 offsetBetweenPlayerAndCamera;
+    private Vector3 curentOffsetBetweenPlayerAndWestWall;
+    private Vector3 initOffsetBetweenPlayerAndWestWall;
+    #endregion
+
+    #region Constants
+    const float MaxCameraXRotationAngle = 0.5f;
+    const float CameraUpMovemetTriggerLimit = 0;
+    #endregion
+
+    #region Variables
+    float cameraMovementRate = 0;
+    Vector3 cameraRotationRate = new Vector3(0, 0, 0);
+    int movementDirection;      // -1 down | 0 none | 1 up
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        offsetBetweenPlayerAndCamera = transform.position - Player.transform.position;
+         initOffsetBetweenPlayerAndWestWall = curentOffsetBetweenPlayerAndWestWall = Player.transform.position - WestWall.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            movementDirection = -1;
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            movementDirection = 1;
+        else movementDirection = 0;
+
+        AlignCamera();
+        RotateCamera();
 	}
+
+    private void RotateCamera()
+    {
+        if (Player.transform.position.z < CameraUpMovemetTriggerLimit &&
+            this.transform.rotation.x < MaxCameraXRotationAngle &&
+            movementDirection==-1)
+        {
+            cameraRotationRate.x = cameraMovementRate;
+            transform.Rotate(cameraRotationRate);
+        }
+
+        else if(Player.transform.position.z < CameraUpMovemetTriggerLimit &&
+            this.transform.rotation.x <= MaxCameraXRotationAngle &&
+            movementDirection==1)
+        {
+            cameraRotationRate.x = -cameraMovementRate;
+            transform.Rotate(cameraRotationRate);
+        }
+    }
+
+    private void AlignCamera()
+    {
+        transform.position = Player.transform.position + offsetBetweenPlayerAndCamera;
+        if (Player.transform.position.z<CameraUpMovemetTriggerLimit)
+        {
+            curentOffsetBetweenPlayerAndWestWall = Player.transform.position - WestWall.transform.position;
+            cameraMovementRate = (initOffsetBetweenPlayerAndWestWall.z - curentOffsetBetweenPlayerAndWestWall.z) / 2;
+            transform.position += new Vector3(0, cameraMovementRate / 2, cameraMovementRate * 3);
+        }
+    }
 }
