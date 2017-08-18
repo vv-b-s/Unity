a9ba5d7760333a9a7b4e7f4cbea46905b39c72a6
vv-b-s <valentin.v1997@gmail.com>
2017-08-18 23:45:22 +0300

    Wrote the board rendering scripts

--- 2D Roguelike tutorial/Assets/Scripts/GameManager.cs
+++ 2D Roguelike tutorial/Assets/Scripts/GameManager.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public BoardManager BoardScript;                 // Keeps the instance of the randomly generated map
+    public static GameManager Instance = null;      // Singleton instance of GameManager. Only one game manager is needed.
+
+    private int level = 5;
+    private void Awake()
+    {
+        // Checking if instance of GameManager allready if not it gives it the value of this instance, if it has instance but is not this, it destroys the object of other instances
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        BoardScript = GetComponent<BoardManager>(); //Get a component reference to the attached BoardManager script
+        InitGame();
+    }
+    /// <summary>
+    /// Initialize BoardManager and game components
+    /// </summary>
+    private void InitGame()
+    {
+        BoardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
