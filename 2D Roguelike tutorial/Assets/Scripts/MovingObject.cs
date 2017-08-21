d6a9bc8352df799ed38134e53f2b6564dbb45ca4
vv-b-s <valentin.v1997@gmail.com>
2017-08-21 17:48:09 +0300

    Created script for the moving object -no  comments added to it yet.

---
+++ 2D Roguelike tutorial/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float MoveTime = 0.1f;
+    public LayerMask BlockLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / MoveTime;
+    }
+
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, BlockLayer);
+        boxCollider.enabled = true;
+
+        if(hit.transform==null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement(Vector2 end)
+    {
+        float sqrRemainingDistance = ((Vector2)transform.position - end).sqrMagnitude;
+
+        while(sqrRemainingDistance>float.Epsilon)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = ((Vector2)transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove<T>(int xDir, int yDir) where T:Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    protected abstract void OnCantMove<T>(T component) where T:Component;
+}
