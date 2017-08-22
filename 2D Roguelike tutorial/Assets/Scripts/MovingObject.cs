efbc812f08b5971377361a1ef3b8976fe78f6d41
vv-b-s <valentin.v1997@gmail.com>
2017-08-22 16:11:03 +0300

    Added comments on MovingObject class

--- 2D Roguelike tutorial/Assets/Scripts/MovingObject.cs
+++ 2D Roguelike tutorial/Assets/Scripts/MovingObject.cs
@@ -2,65 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//The abstract keyword enables you to create classes and class members that are incomplete and must be implemented in a derived class.
 public abstract class MovingObject : MonoBehaviour
 {
-    public float MoveTime = 0.1f;
-    public LayerMask BlockLayer;
+    public float MoveTime = 0.1f;           //Time it will take object to move, in seconds.
+    public LayerMask BlockLayer;           //Layer on which collision will be checked.
 
-    private BoxCollider2D boxCollider;
-    private Rigidbody2D rb2D;
-    private float inverseMoveTime;
+    private BoxCollider2D boxCollider;   //The BoxCollider2D component attached to this object.
+    private Rigidbody2D rb2D;           //The Rigidbody2D component attached to this object.
+    private float inverseMoveTime;     //Used to make movement more efficient.
 
+    //Protected, virtual functions can be overridden by inheriting classes.
     protected virtual void Start()
     {
+        // Get a component reference to this object's BoxCollider2D
         boxCollider = GetComponent<BoxCollider2D>();
+
+        //Get a component reference to this object's Rigidbody2D
         rb2D = GetComponent<Rigidbody2D>();
+
+        //By storing the reciprocal of the move time we can use it by multiplying instead of dividing, this is more efficient.
         inverseMoveTime = 1f / MoveTime;
     }
 
+    /// <summary>
+    /// Returns true if it is able to move and false if not.
+    /// Takes x and y directions and RaycastHit2D to check collision.
+    /// </summary>
+    /// <param name="xDir"></param>
+    /// <param name="yDir"></param>
+    /// <param name="hit"></param>
+    /// <returns></returns>
     protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
     {
+        //Store start position to move from, based on objects current transform position.
         Vector2 start = transform.position;
+
+        //Store start position to move from, based on objects current transform position.
         Vector2 end = start + new Vector2(xDir, yDir);
 
+
+        //Disable the boxCollider so that linecast doesn't hit this object's own collider.
         boxCollider.enabled = false;
+
+        //Cast a line from start point to end point checking collision on blockingLayer.
         hit = Physics2D.Linecast(start, end, BlockLayer);
+
+        //Re-enable boxCollider after linecast
         boxCollider.enabled = true;
 
-        if(hit.transform==null)
+        //Check if anything was hit
+        if (hit.transform==null)
         {
+            //If nothing was hit, start SmoothMovement co-routine passing in the Vector2 end as destination
             StartCoroutine(SmoothMovement(end));
+
+            //Return true to say that Move was successful
             return true;
         }
 
+        //If something was hit, return false, Move was unsuccesful.
         return false;
     }
 
+    /// <summary>
+    /// Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
+    /// </summary>
+    /// <param name="end"></param>
+    /// <returns></returns>
     protected IEnumerator SmoothMovement(Vector2 end)
     {
+        //Calculate the remaining distance to move based on the square magnitude (величината) of the difference between current position and end parameter. 
+        //Square magnitude is used instead of magnitude because it's computationally cheaper.
         float sqrRemainingDistance = ((Vector2)transform.position - end).sqrMagnitude;
 
-        while(sqrRemainingDistance>float.Epsilon)
+        //While that distance is greater than a very small amount (Epsilon, almost zero):
+        while (sqrRemainingDistance>float.Epsilon)
         {
+            //Find a new position proportionally closer to the end, based on the moveTime
             Vector2 newPosition = Vector2.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+
+            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
             rb2D.MovePosition(newPosition);
+
+            //Recalculate the remaining distance after moving.
             sqrRemainingDistance = ((Vector2)transform.position - end).sqrMagnitude;
+
+            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
             yield return null;
         }
     }
 
+    /// <summary>
+    /// AttemptMove takes a generic parameter T to specify the type of component we expect our unit to interact with if blocked (Player for Enemies, Wall for Player).
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="xDir"></param>
+    /// <param name="yDir"></param>
     protected virtual void AttemptMove<T>(int xDir, int yDir) where T:Component
     {
+        //Hit will store whatever our linecast hits when Move is called.
         RaycastHit2D hit;
+
+        //Set canMove to true if Move was successful, false if failed.
         bool canMove = Move(xDir, yDir, out hit);
 
+        //Check if nothing was hit by linecast
         if (hit.transform == null)
             return;
+
+        //Get a component reference to the component of type T attached to the object that was hit
         T hitComponent = hit.transform.GetComponent<T>();
 
+        //If canMove is false and hitComponent is not equal to null, meaning MovingObject is blocked and has hit something it can interact with.
         if (!canMove && hitComponent != null)
-            OnCantMove(hitComponent);
+            OnCantMove(hitComponent);             //Call the OnCantMove function and pass it hitComponent as a parameter.
     }
 
+    //OnCantMove will be overriden by functions in the inheriting classes.
     protected abstract void OnCantMove<T>(T component) where T:Component;
 }
