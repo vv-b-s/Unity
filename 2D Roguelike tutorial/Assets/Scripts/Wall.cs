819c9a779595932c3565295752db3ae55af9af1f
vv-b-s <valentin.v1997@gmail.com>
2017-08-25 21:48:10 +0300

    created destructable walls script and added configured plaer animation

---
+++ 2D Roguelike tutorial/Assets/Scripts/Wall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite DmgSprite;
+    public int Hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamaageWall (int loss)
+    {
+        spriteRenderer.sprite = DmgSprite;
+        Hp -= loss;
+        if (Hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
